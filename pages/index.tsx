d438cc1203b3033623904198013c0a6cdff5d954
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-10 09:33:23 +0900

    構成変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Reactが大好きです。</p>
+        <p>Reactが好きです。</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
